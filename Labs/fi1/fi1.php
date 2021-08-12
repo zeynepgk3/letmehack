e83942e2049ccdde9313547f9658289d44a9a680
omerrkeles <85849392+omerrkeles@users.noreply.github.com>
2021-08-12 14:17:00 +0300

    img-css
    labs mevzuları

--- Labs/fi1/fi1.php
+++ Labs/fi1/fi1.php
@@ -1,6 +1,26 @@
 <html>
 
 <head>
+    
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/fi.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script src="https://kit.fontawesome.com/6444dbd282.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
 
     <title>File Inclusion LAB 1</title>
 
@@ -8,21 +28,71 @@
 
 <body>
 
-<h1>Harry Potter</h1>
 
 
-<form method="get">
+
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+    <div class="container">
+
+      <!-- Brand -->
+      <a class="navbar-brand" href="" target="_blank">
+        <strong>Harry Potter</strong>
+      </a>
+
+      <!-- Collapse -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Links -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <!-- Left -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="" target="_blank">About Harry Potter</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="" target="_blank">Watch</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="" target="_blank">Another Films</a>
+          </li>
+        </ul>
+
+      
+
+      </div>
+
+    </div>
+  </nav>
+
+
+
+
+
+
+
+<form method="get" class=omer >
 
    <select name="filmler">
 
-      <option value="film1">Felsefe Tasi</option>
-      <option value="film2">Sirlar Odasi</option>
-      <option value="film3">Azkaban Tutsagi</option>
-      <option value="film4">Ates Kadehi</option>
+      <option value="film1">Philosopher's Stone</option>
+      <option value="film2">Chamber of Secrets </option>
+      <option value="film3">Prisoner of Azkaban</option>
+      <option value="film4"> Goblet of Fire</option>
 
    </select>
 
-   <input type="submit">
+   
+   <button type="submit"><i class="fa fa-search renk"></i></button>
+   
 
 </form>
 
--- Labs/xss1/xss1.php
+++ Labs/xss1/xss1.php
@@ -5,20 +5,55 @@ XSS LAB 1
 -->
 <html>
 <head>
-<title>XSS LAB 111111</title>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="lab1/css/lab1.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script src="https://kit.fontawesome.com/6444dbd282.js" crossorigin="anonymous"></script>
+
+
+<title>XSS LAB 1</title>
 </head>
 <body>
-<form action='xss1.php' method='POST' id="xss1">
+
+<div class="adam">
+      <img   src="lab1/img/logo.svg" alt="Love"  >
+        </div>
+
+<div class="calcu">
+        <p style="text-align: center"> Example:  Marie John</p> 
+</div>
+
+
+<form class="calcu"; action='lab1.php' method='POST' id="xss1">
 <input type="text" id="xss" name="xss1-1"><br><br>
-<input type="submit" value="Submit">
+<input type="submit" class="love " value="Calculate Love">
 </form>
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['xss1-1'];
     if (empty($name)) {
-        echo "Name is empty";
+        echo "Name is empty. Try AGAİN!";
     } else {
         echo $name;
+        echo "    " . " You are the Perfect couple"; 
+        
+            
+            
+        
+      
     }
 }
 ?>
